7f17c87a96e50e77ba444ba6b63feb5aa39dd71d
C. Edward Arc <101595385+cedwardarc@users.noreply.github.com>
2022-09-23 09:43:18 -0300

    Create botão+-.js

---
+++ botão+-.js
@@ -0,0 +1,15 @@
+let tamanho = 2;
+function atutam() {
+    const h1 = document.querySelector("h1");
+    h1.style.fontSize = '${tamanho}em';
+}
+function atituto() {
+    tamanho += 0.5;
+    atutam();
+}
+function dtituto() {
+    if(tamanho > 0.5) {
+        tamanho -= 0.5;
+        atutam();
+    }
+}
